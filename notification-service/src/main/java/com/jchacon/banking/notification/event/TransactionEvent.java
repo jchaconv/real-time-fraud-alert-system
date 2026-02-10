ea5092cb2491895f28eaa645a17c5942fa1c6af4
Julio Chacon Vilela <jchacon.vilela@gmail.com>
2026-02-10 17:04:40 -0500

    02-notification-service initial config of the project

---
+++ notification-service/src/main/java/com/jchacon/banking/notification/event/TransactionEvent.java
@@ -0,0 +1,23 @@
+package com.jchacon.banking.notification.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionEvent {
+    private String transactionId;
+    private String customerId;
+    private BigDecimal amount;
+    private String status;
+    private String responseCode;
+    private LocalDateTime timestamp;
+    private String correlationId;
+}
