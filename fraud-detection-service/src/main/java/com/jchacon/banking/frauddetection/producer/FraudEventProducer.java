ed69543bb0b679e2ff800c98267e6555ba1bb525
Julio Chacon Vilela <jchacon.vilela@gmail.com>
2026-02-11 12:19:23 -0500

    05-kafka-retries-backoff retries and backoff test ok - kafka server port wrong changed

--- fraud-detection-service/src/main/java/com/jchacon/banking/frauddetection/producer/FraudEventProducer.java
+++ fraud-detection-service/src/main/java/com/jchacon/banking/frauddetection/producer/FraudEventProducer.java
@@ -3,9 +3,14 @@ package com.jchacon.banking.frauddetection.producer;
 import com.jchacon.banking.frauddetection.event.TransactionEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.errors.NetworkException;
+import org.apache.kafka.common.errors.TimeoutException;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Slf4j
 @Component
@@ -22,11 +27,27 @@ public class FraudEventProducer {
      */
     public Mono<Void> sendTransactionEvent(TransactionEvent event) {
         return Mono.fromFuture(() -> kafkaTemplate.send(TOPIC, event.getTransactionId(), event))
+                .retryWhen(Retry.backoff(3, Duration.ofMillis(500)) // 3 retries, starting at 500ms
+                        .jitter(0.75) // Avoid "thundering herd" problem
+                        .filter(this::isRecoverable) // Only retry on technical errors
+                        .doBeforeRetry(retrySignal -> log.warn("Retrying Kafka send... Attempt: {}", retrySignal.totalRetries() + 1))
+                )
                 .doOnSuccess(result -> log.info("Event sent to Kafka: {} | Topic: {} | Partition: {}",
                         event.getTransactionId(),
                         result.getRecordMetadata().topic(),
                         result.getRecordMetadata().partition()))
-                .doOnError(e -> log.error("Error sending to Kafka", e))
+                .onErrorResume(e -> {
+                    log.error("CRITICAL: Kafka failed after retries for TXN {}: {}",
+                            event.getTransactionId(), e.getMessage());
+                    // In banking, here you might save to a "Dead Letter Table" in DB
+                    return Mono.empty();
+                })
                 .then();
+    }
+
+    private boolean isRecoverable(Throwable e) {
+        // Only retry on timeouts or connection issues, not on business or serialization errors
+        return e instanceof TimeoutException ||
+                e instanceof NetworkException;
     }
 }
